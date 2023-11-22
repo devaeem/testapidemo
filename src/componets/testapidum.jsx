e6821ee6d431b2ba2e8964485d4d6aec1b805fe0
tattapan <tattapan.wan@everapp.io>
2023-11-22 14:13:35 +0700

    add api and tool

--- src/componets/testapidum.jsx
+++ src/componets/testapidum.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
+import  axios  from "axios";
 const testapidum = () => {
-  return (
-    <>testapidum</>
-  )
-}
+  const [data, setData] = useState([]);
+  const [btn, setBtn] = useState('');
 
-export default testapidum
+
+  useEffect(()=>{
+
+    loadData(btn);
+  },[])
+//   const loadData = () => {};
+  const handleAdd = (e) => {
+    // console.log(e.target.value);
+    setBtn(e.target.value);
+  };
+  const handleBtn = (btn) => {
+    if(!btn){
+      return  alert("NOT DATA!!!") 
+    }
+    loadData();
+   
+  };
+
+
+  const loadData = async ()=>{
+   await axios.get(`https://dummyjson.com/products?limit=${btn}`)
+   .then((res) => {
+    console.log(res.data.products)
+     setData(res.data.products);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+  }
+const handleReset =()=>{
+    setData([]);
+    setBtn(0); 
+}
+  
+
+  return (
+    <>
+      <div className="test-api-dummy-container">
+      <input type="number" onChange={handleAdd} value={btn} />
+      <button className="btn-add" onClick={handleBtn}>
+        ADD
+      </button>
+      <button className="btn-reset" onClick={handleReset}>
+        RESET
+      </button>
+      <ul className="data-list">
+        {data.map((item) => (
+          <li key={item.id}>{item.title}</li>
+        ))}
+      </ul>
+    </div>
+       
+       
+     
+    </>
+  );
+};
+
+export default testapidum;
